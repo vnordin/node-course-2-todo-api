1ca5833e9cef180947f95230ef19c6802aee389a
vnordin <vnordin@se.ibm.com>
2018-12-05 14:18:51 +0100

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -21,11 +22,30 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req,res) => {
+app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    //req.params.is kommer innehålla det vi skickar med i urlen (requesten).
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+           return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
@@ -33,4 +53,4 @@ app.listen(3000, () => {
     console.log('Started on port 3000')
 });
 
-module.exports = {app};
+module.exports = { app };
