bde5907522f45f12028404c16d2d62152e4a1e64
vnordin <vnordin@se.ibm.com>
2018-12-06 12:05:18 +0100

    updated mongo db source

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://admin:admin123@ds153413.mlab.com:53413/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
